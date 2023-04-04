3499cb317596829cac541f8baf126d1062273d21
Toran Sahu <toran.sahu@yahoo.com>
2023-04-04 12:02:52 +0530

    Add a hello world python script

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# created_on: 2023-04-03 12:29
+
+"""Hello World."""
+
+
+__author__ = 'Toran Sahu <toran.sahu@yahoo.com>'
+__license__ = 'Distributed under terms of the MIT license'
+
+
+for _ in range(10):
+    print("Hello World")
